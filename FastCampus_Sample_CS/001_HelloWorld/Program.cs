f0707098e912d1ac746244f9926c84279c8b8e38
LOHHAMILTON <tttggg963@naver.com>
2022-10-30 10:44:45 +0900

    HelloWorld

---
+++ FastCampus_Sample_CS/001_HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*c#배우기 메인 1강*/
+
+namespace _001_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)// 함수의 속성 , 리턴형, 함수의 이름
+        {
+            Console.WriteLine("HelloWorld");
+        }
+    }
+}
